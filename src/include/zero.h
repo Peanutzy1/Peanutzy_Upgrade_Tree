9579836311677adfe4215a09f36f56db62152d64
peanutzy <hotara1025@gmail.com>
2026-01-02 14:29:53 +0700

    basic slab struct

---
+++ src/include/zero.h
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#ifndef ZERO_SLAB
+#define ZERO_SLAB
+typedef struct {
+
+    float cam_x;
+    float cam_y;
+    float mouse_x;
+    float mouse_y;
+
+    
+} ZeroSlab;
+#endif
