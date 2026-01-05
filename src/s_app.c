8a8bf7cad3bc60b03782dc2d450f711c335839f1
peanutzy <hotara1025@gmail.com>
2026-01-05 15:37:06 +0700

    more sokol and a wrapper file

--- src/s_app.c
+++ src/s_app.c
@@ -1,4 +1,22 @@
-//
-// Created by peanutzy on 1/4/26.
-//
+/*
+ * This file makes a sokol app
+ * I hate cross-platform compatibility
+ */
 
+#define SOKOL_IMPL
+#include "big_bad_sokol_headers/sokol_app.h"
+#include "big_bad_sokol_headers/sokol_gfx.h"
+#include "big_bad_sokol_headers/sokol_glue.h"
+
+static struct
+{
+    sg_pass_action pass_action;
+} state;
+
+void init(void)
+{
+  sg_setup(&(sg_desc){
+    .enviroment = sglue_enviroment(),
+    .logger.func = slog_func
+  });
+}
